b56ef3aff7e713c4c58405eb79825599be3d2e15
Andre Mekkawi <github@andremekkawi.com>
2012-10-11 16:07:38 -0400

    Changed decompile.sh to be more OO-like. Also added ignores.

--- decompile.sh
+++ decompile.sh
@@ -11,8 +11,20 @@ function main() {
 	
 	local doclient=true
 	local doserver=true
+	local branch=
 	
-	local branch="$(cd "$SCRIPTDIR/$CLIENT_SRC_PROJECT" 2> /dev/null && git symbolic-ref -q HEAD 2> /dev/null)"
+	check_client_src_project() || EXITCLEAN $?
+	check_server_src_project() || EXITCLEAN $?
+	
+	if $doserver || $doclient; then
+		echo
+		cd "$SCRIPTDIR/$MCP"
+		"$PYCMD" "$scriptpath" -r -c "$cfgpath"
+	fi
+}
+
+check_client_src_project() {
+	branch="$(cd "$SCRIPTDIR/$CLIENT_SRC_PROJECT" 2> /dev/null && git symbolic-ref -q HEAD 2> /dev/null)"
 	if [ "$branch" != "refs/heads/$CLIENT_SRC_DECOMPILEBRANCH" ]; then
 		if [ "$branch" == "" ]; then
 			echo "SKIPPING CLIENT: Either $CLIENT_SRC_PROJECT is not a git repo or it is not on the refs/heads/$CLIENT_SRC_DECOMPILEBRANCH branch."
@@ -22,6 +34,20 @@ function main() {
 		doclient=false
 	fi
 	
+	if $doclient && [ -d "$SCRIPTDIR/$CLIENT_SRC_PROJECT/src" ]; then
+		(cd "$SCRIPTDIR/$CLIENT_SRC_PROJECT" && git status -u --ignored --porcelain 2> /dev/null > "$TEMPDIR/gitstatus")
+		[ $? -ne 0 ] && echo "ERROR: Could not get a list of untracked/ignored files for: $CLIENT_SRC_PROJECT" && return 1
+		
+		statuslines="$(grep -v '!! bin/' "$TEMPDIR/gitstatus" | wc -l | tr -d '\t ')"
+		[ "$statuslines" != "0" ] && echo "ERROR: One or more untracked or ignored files are in: $CLIENT_SRC_PROJECT" && echo "       You must remove or stash them. See: git status -u --ignored" && return 1
+		
+		echo "Removing $CLIENT_SRC_PROJECT/src..."
+		rm -rf "$SCRIPTDIR/$CLIENT_SRC_PROJECT/src"
+		[ $? -ne 0 ] && echo "ERROR: Could not remove old src directory." && return 1
+	fi
+}
+
+check_server_src_project() {
 	branch="$(cd "$SCRIPTDIR/$SERVER_SRC_PROJECT" 2> /dev/null && git symbolic-ref -q HEAD 2> /dev/null)"
 	if [ "$branch" != "refs/heads/$SERVER_SRC_DECOMPILEBRANCH" ]; then
 		if [ "$branch" == "" ]; then
@@ -32,35 +58,25 @@ function main() {
 		doserver=false
 	fi
 	
-	if $doclient && [ -d "$SCRIPTDIR/$CLIENT_SRC_PROJECT/src" ]; then
-		(cd "$SCRIPTDIR/$CLIENT_SRC_PROJECT" && git status -u --ignored --porcelain 2> /dev/null > "$TEMPDIR/gitstatus")
-		[ $? -ne 0 ] && echo "ERROR: Could not get a list of untracked/ignored files for: $CLIENT_SRC_PROJECT" && EXITCLEAN 1
-		
-		statuslines="$(grep -v '!! bin/' "$TEMPDIR/gitstatus" | wc -l | tr -d '\t ')"
-		[ "$statuslines" != "0" ] && echo "ERROR: One or more untracked or ignored files are in: $CLIENT_SRC_PROJECT" && echo "       You must remove or stash them. See: git status -u --ignored" && EXITCLEAN 1
-		
-		echo "Removing $CLIENT_SRC_PROJECT/src..."
-		rm -rf "$SCRIPTDIR/$CLIENT_SRC_PROJECT/src"
-		[ $? -ne 0 ] && echo "ERROR: Could not remove old src directory." && EXITCLEAN 1
-	fi
-	
 	if $doserver && [ -d "$SCRIPTDIR/$SERVER_SRC_PROJECT/src" ]; then
 		(cd "$SCRIPTDIR/$SERVER_SRC_PROJECT" && git status -u --ignored --porcelain 2> /dev/null > "$TEMPDIR/gitstatus")
-		[ $? -ne 0 ] && echo "ERROR: Could not get a list of untracked/ignored files for: $SERVER_SRC_PROJECT" && EXITCLEAN 1
+		[ $? -ne 0 ] && echo "ERROR: Could not get a list of untracked/ignored files for: $SERVER_SRC_PROJECT" && return 1
 		
 		statuslines="$(grep -v '!! bin/' "$TEMPDIR/gitstatus" | wc -l | tr -d '\t ')"
-		[ "$statuslines" != "0" ] && echo "ERROR: One or more untracked or ignored files are in: $SERVER_SRC_PROJECT" && echo "       You must remove or stash them. See: git status -u --ignored" && EXITCLEAN 1
+		[ "$statuslines" != "0" ] && echo "ERROR: One or more untracked or ignored files are in: $SERVER_SRC_PROJECT" && echo "       You must remove or stash them. See: git status -u --ignored" && return 1
 		
 		echo "Removing $SERVER_SRC_PROJECT/src..."
 		rm -rf "$SCRIPTDIR/$SERVER_SRC_PROJECT/src"
-		[ $? -ne 0 ] && echo "ERROR: Could not remove old src directory." && EXITCLEAN 1
+		[ $? -ne 0 ] && echo "ERROR: Could not remove old src directory." && return 1
 	fi
+}
+
+do_client() {
 	
-	if $doserver || $doclient; then
-		echo
-		cd "$SCRIPTDIR/$MCP"
-		"$PYCMD" "$scriptpath" -r -c "$cfgpath"
-	fi
+}
+
+do_server() {
+	
 }
 
 main "$@"
